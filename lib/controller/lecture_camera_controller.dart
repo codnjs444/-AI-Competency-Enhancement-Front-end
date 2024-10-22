566c220dae89886f56870183b5634c636a1c32b3
Chae-Won Lee <81910499+codnjs444@users.noreply.github.com>
2024-10-22 15:19:21 +0900

    서버 연결 및 스크린 캡쳐 기능

--- lib/controller/lecture_camera_controller.dart
+++ lib/controller/lecture_camera_controller.dart
@@ -1,58 +1,115 @@
 import 'dart:typed_data';
-import 'package:camera/camera.dart';
+import 'dart:convert';
 import 'package:get/get.dart';
+import 'package:camera/camera.dart';
 import 'package:screenshot/screenshot.dart';
 import 'package:image/image.dart' as IMG;
+import 'package:http/http.dart' as http;
 
 class LectureCameraController extends GetxController {
-  LectureCameraController get to => Get.find();
+LectureCameraController get to => Get.find();
 
-  late List<CameraDescription> cameras;
-  late CameraController camerasController;
-  bool cameraInitChk = false; //카메라 초기화 체크
+late List<CameraDescription> cameras;
+late CameraController camerasController;
+bool cameraInitChk = false; //카메라 초기화 체크
+//스크린 샷
+late ScreenshotController screenshotController;
+//이미지 높이와 넓이
+late int loadingChk = 0;
+late int widget_height = 0;//위젯 높이
+late int widget_width = 0;//위젯 넓이
 
-  //스크린 샷
-  late ScreenshotController screenshotController;
-  //이미지 높이와 넓이
-  late int widget_height = 0;//위젯 높이
-  late int widget_width = 0;//위젯 넓이/
+//서버에서 가져오는 데이터 리스트
+late List<String> dataList;
+late int dataListCnt;
 
-  @override
-  void onInit() async{
-    //딱 한번만 페이지 들어 올때 실행 합니다.
-    initCamera();
-    super.onInit();
-  }
+@override
+void onInit() async {
+initCamera();
+//페이지 진입시 한번만 실행
+screenshotController = ScreenshotController();
+super.onInit();
+}
 
-  //카메라 초기화
-  Future<void> initCamera() async {
-    cameras = await availableCameras(); //기다려줘 카메라 가져올때까지
+//카메라 초기화
+Future<void> initCamera() async {
+cameras = await availableCameras(); //기다려줘 카메라 가져올때까지
+camerasController = CameraController(cameras[0], ResolutionPreset.max, enableAudio : false) ;
+//카메라 초기화
+camerasController.initialize().then((_) {// 카메라 사용 가능 상태로 왔 습니다.
+cameraInitChk = true;
+update();//화면에 값을 전달
+});
+}
 
-    camerasController = CameraController(cameras[0], ResolutionPreset.max, enableAudio : false) ;
-    //카메라 초기화
-    camerasController.initialize().then((_) {// 카메라 사용 가능 상태로 왔 습니다.
-      cameraInitChk = true;
-      update();//화면에 값을 전달
-    });
-  }
+//화면 캡쳐
+Future<void> clickPicture(Uint8List image) async {
+print('## clickPicture 시작 ##');
 
-  //화면 캡쳐
-  Future<void> clickPicture(Uint8List image) async {
-    print('## clickPicture 시작 ##');
+List<Uint8List>? PictureImgList = <Uint8List>[];
+IMG.Image ? img = IMG.decodeImage(image);
+IMG.Image resized = IMG.copyResize(img!, width: widget_width, height: widget_height);
+image = IMG.encodeJpg(resized);
 
-    List<Uint8List>? PictureImgList = <Uint8List>[];
-    IMG.Image ? img = IMG.decodeImage(image);
-    IMG.Image resized = IMG.copyResize(img!, width: widget_width, height: widget_height);
-    image = IMG.encodeJpg(resized);
+print('widget_width : ${widget_width}');
+print('widget_height : ${widget_height}');
 
-    print('widget_width : ${widget_width}');
-    print('widget_height : ${widget_height}');
+PictureImgList.add(image);
+print('## clickPicture 종료 ##');
 
-    PictureImgList.add(image);
-    print('## clickPicture 종료 ##');
+httpMultipartCall(PictureImgList); //서버로 이미지를 보내서 체크 하도록 합니다. (아래 httpMultipartCall 메서드 로 옵니다)
 
-    //httpMultipartCall(PictureImgList); //서버로 이미지를 보내서 체크 하도록 합니다. (아래 httpMultipartCall 메서드 로 옵니다)
+}
 
-  }
+/*서버 AI 체크 */
+  String serverUrl = 'http://172.17.16.1:8081';//각자 로컬 IP
+String mapping = '/getAiData';
+Future<Map<String, dynamic>> ? httpMultipartCall(List<Uint8List> pictureImgList) async {
+print('## httpMultipartCall 시작 ##');
+late Map<String, dynamic> resultData ={};//반환 데이터
+
+String dbSelectUrl = serverUrl+mapping;
+try {
+var request = http.MultipartRequest("POST", Uri.parse(dbSelectUrl),);
+for (var i = 0; i < pictureImgList.length; i++) {
+request.files.add(await http.MultipartFile.fromBytes('test_${i + 1}', pictureImgList[i],filename: 'test_${i + 1}'),);
+}
+
+var response = await request.send();//위에 이미지 를 서버로 보낸다.
+
+if (response.statusCode == 200) {
+print('############ httpMultipartCall 서버데이터 호출 완료 #########');
+
+var responseSend = await http.Response.fromStream(response);
+resultData = jsonDecode(utf8.decode(responseSend.bodyBytes));
+
+print('responseData resultList: ${resultData['resultList']}');
+
+String serverData = resultData['resultList'].toString().trim();
+
+String list = resultData['list'].toString().trim();
+String pk = resultData['pk'].toString().trim();
+String cm = resultData['cm'].toString().trim();
+
+
+
+
+if(serverData.length > 2){
+serverData = serverData.substring(1,serverData.length-1);
+dataList = serverData.split(',');
+print('dataList: ${dataList.length}');
+dataListCnt = dataList.length;
+}else{
+dataListCnt =0;
+}
+loadingChk = 2;
+update();
+}
+print('## httpMultipartCall 종료 ##');
+} catch (e) {
+print('httpMultipartCall 서버 에러 : ${e}');
+}
+return resultData;
+}
 
 }
--- lib/views/lecture_camera.dart
+++ lib/views/lecture_camera.dart
@@ -1,39 +1,76 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lecture/controller/lecture_camera_controller.dart';
 import 'package:camera/camera.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:widget_size/widget_size.dart';
 
 class LectureCamera extends GetView<LectureCameraController> {
+  LectureCamera({super.key});
+
   @override
   Widget build(BuildContext context) {
+    print('카메라 화면');
     return GetBuilder<LectureCameraController>(
         init: controller,
         builder: (_) {
-          return Scaffold(
-              appBar: AppBar(
-                backgroundColor: Colors.lightBlue[100],
-                actions: [
-                  TextButton(
+          return Stack(
+              children: [
+                WidgetSize(onChange: (Size size){
+                  int temp_height = size.height.round();
+                  int temp_width = size.width.round();
+                  //화면 사이즈 전달
+                  controller.widget_height = temp_height;
+                  controller.widget_width = temp_width;
+                },
+                    child:
+                    Screenshot(
+                        controller : controller.screenshotController,
+                        child : Scaffold(
+                          appBar: AppBar(
+                            title: const Text('강의 학습 로드'),
+                          ),
+                          body: controller.cameraInitChk == false ?
+                          const SizedBox.shrink()
+                              :Stack(
+                            children: [
+                              Form(
+                                  canPop: false,
+                                  onPopInvokedWithResult: (bool didPop, Object? result){
+                                    if(!didPop){
+                                      controller.camerasController.dispose();
+                                      Get.back();
+                                      return;
+                                    }
+                                  },
+                                  child: const SizedBox.shrink()
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                height: MediaQuery.of(context).size.height,
+                                child: CameraPreview(controller.camerasController),
+                              )
+                            ],
+                          ),
+                          floatingActionButton: FloatingActionButton(
+                            onPressed: ()  {
+                              controller.loadingChk = 1;
+                              controller.update();
 
-                      onPressed: (){
-                        Get.toNamed('/lecture_camera');
-                      },
-                      child: const Icon(Icons.camera_alt_outlined, color : Colors.black54)
-                  )
-                ],
-              ),
-              body: controller.cameraInitChk == false ?
-              const SizedBox.shrink()
-                  : Stack(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height,
-                      child: CameraPreview(controller.camerasController),
+                              controller.screenshotController.capture().then((Uint8List ? capturedImage) {
+                                if(capturedImage != null){
+                                  controller.clickPicture(capturedImage);
+                                }
+                              });
+                            },
+                            child: const Text('Click'),
+                          ),
+                        )
                     )
-                  ]
-              )
+                ),
 
+              ]
           );
         }
     );
